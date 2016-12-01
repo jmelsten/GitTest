f01a3d486a8ae90cc209017f3fcdd761675f788f
jmelsten <j.m.elsten@student.utwente.nl>
2016-12-01 15:26:30 +0100

    Second False Commit.

--- GitTest/src/Git/Hello.java
+++ GitTest/src/Git/Hello.java
@@ -1,6 +1,10 @@
 package Git;
 
 public class Hello {
-	public static void main(String[] args)
-		System . out . print ln ("Hello ,world! " );
+
+	public static void main(String[] args) {
+		System.out.println("Hallo, World!");
+
+	}
+
 }
